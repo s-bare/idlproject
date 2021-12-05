148c86fddb0a7be48ceacfbd7520e120d4c2bcc4
supreethbare <bare.supreeth@gmail.com>
2021-12-05 17:21:15 -0500

    Initial Push

---
+++ idlproject.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+import pydeck as pdk
+
+st.set_page_config(layout="wide")
+
+st.title("""UNDERSTANDING POLITICAL POLARISATION USING NLP""")
+
+st.header('TESTING')
